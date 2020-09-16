540783491e0cd850d6d964d50916e1276a1a56c0
Gonzalo Flores <gonzaflores358@gmail.com>
2020-09-16 19:50:15 -0300

    añadi funcionalidad del horario y momento del dia

--- src/components/current/current.js
+++ src/components/current/current.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
 import './current.css';
 
+function uppperFirst(string) {
+  return string[0].toUpperCase() + string.substring(1);
+}
+
+function round(num, decimales = 2) {
+  var signo = num >= 0 ? 1 : -1;
+  num = num * signo;
+  if (decimales === 0) {
+    //con 0 decimales
+    return signo * Math.round(num);
+  }
+  // round(x * 10 ^ decimales)
+  num = num.toString().split('e');
+  num = Math.round(
+    +(num[0] + 'e' + (num[1] ? +num[1] + decimales : decimales))
+  );
+  // x * 10 ^ (-decimales)
+  num = num.toString().split('e');
+  return signo * (num[0] + 'e' + (num[1] ? +num[1] - decimales : -decimales));
+}
+
 class CurrentWeather extends Component {
   render() {
     const { city } = this.props;
     const { temp, weather } = this.props.current;
-    console.log(weather);
-    const condition = /* weather ? weather[0].description :  */ '';
+    const { description, icon } = weather[0];
     return (
       <div className="current-weather">
-        <p className="where">Tiempo en {city}</p>
-        <h2 className="temp">{temp} °C</h2>
-        <p className="condition">{condition}</p>
+        <div className="info">
+          <p className="where">Tiempo en {city}</p>
+          <h2 className="temp">{round(temp, 1)} °C</h2>
+          <p className="condition">{uppperFirst(description)}</p>
+        </div>
+        <div className="icon">
+          <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} />
+        </div>
       </div>
     );
   }
--- src/views/home.js
+++ src/views/home.js
@@ -2,6 +2,30 @@ import React, { Component } from 'react';
 import getInfoWeatherApi from '../api/api.js';
 import SearchNav from '../components/searchbar/searchnav.js';
 import CurrentWeather from '../components/current/current.js';
+import moment from 'moment';
+
+function getTime(unix, offset) {
+  let utcHours = moment.unix(unix)._d.getUTCHours();
+  let hours = utcHours + offset / 3600;
+
+  while (hours > 24 || hours < 0) {
+    if (hours > 24) {
+      hours -= 24;
+    } else if (hours < 0) {
+      hours += 24;
+    }
+  }
+
+  if (hours >= 5 && hours <= 12) {
+    return { state: 'morning', hours };
+  } else if (hours > 12 && hours < 19) {
+    return { state: 'day', hours };
+  } else if (hours >= 19 && hours < 21) {
+    return { state: 'afternoon', hours };
+  } else if ((hours >= 21 && hours <= 24) || (hours >= 0 && hours < 5)) {
+    return { state: 'night', hours };
+  }
+}
 
 class Home extends Component {
   constructor(props) {
@@ -12,6 +36,7 @@ class Home extends Component {
       current: {},
       hourly: {},
       daily: {},
+      time: 0,
     };
 
     this.getInfoWeather = this.getInfoWeather.bind(this);
@@ -25,28 +50,36 @@ class Home extends Component {
     this.setState({ status: 'searching' });
     getInfoWeatherApi(city, (status, data) => {
       let { current, daily, hourly } = data;
+      const { state, hours } = getTime(current.dt, data.timezone_offset);
       this.setState({
         city,
         status,
         current,
         daily,
         hourly,
+        state,
+        hours,
       });
     });
   }
 
   render() {
-    const { city, current } = this.state;
+    const { city, current, status, state } = this.state;
+    let element;
+
+    if (status === 'searching') {
+      element = <div>Buscando</div>;
+    } else if (status === 200) {
+      element = <CurrentWeather city={city} current={current} />;
+    } else if (status === 404) {
+      element = <div>No encontrado</div>;
+    }
     return (
-      <div>
+      <div className={state}>
         <SearchNav onSearch={this.getInfoWeather} />
         <main>
           <div className="sep"></div>
-          {this.state.status !== 404 ? (
-            <CurrentWeather city={city} current={current} />
-          ) : (
-            '404'
-          )}
+          {element}
         </main>
       </div>
     );
